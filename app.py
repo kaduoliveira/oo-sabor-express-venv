e9e8733eff8c89950d49924821aec264115333d5
Kadu Oliveira <carloseduardo_al@hotmail.com>
2024-09-14 10:26:00 -0300

    mostrando pratos por restaurante

--- app.py
+++ app.py
@@ -6,6 +6,22 @@ print(response)
 
 if response.status_code == 200:
     dados_json = response.json()
-    print(dados_json)
+    dados_restaurante = {}
+    #print(dados_json)
+    for item in dados_json:
+        nome_do_restaurante = item['Company']
+        nome_do_prato = item['Item']
+        preco = item['price']
+        descricao = item['description']
+        if nome_do_restaurante not in dados_restaurante:
+            dados_restaurante[nome_do_restaurante] = []
+        dados_restaurante[nome_do_restaurante].append({
+            'item': item['Item'],
+            'price': item['price'],
+            'description': item['description']
+        })
+    print(dados_restaurante['KFC'])
 else:
     print(f'O erro foi {response.status_code}')
+
+#print(dados_restaurante['Burger King'])
